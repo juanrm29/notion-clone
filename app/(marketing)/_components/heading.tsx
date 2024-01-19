a885aebb912c7d362f574e94c2eecfab679ad3a9
juanrm29 <juanmaulana29@gmail.com>
2024-01-20 03:15:03 +0800

    auth

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -3,8 +3,12 @@
 import { ArrowRight } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -15,10 +19,15 @@ export const Heading = () => {
         Jution is the connected workspace where <br />
         better, faster work happens.
       </h3>
+      {isLoading && (
+        <Spinner size="lg"/>
+      )}
+      {!isAuthenticated && !isLoading && (
       <Button>
         Enter Jution
         <ArrowRight className="h-4 w-4 ml-2" />
       </Button>
+      )}
     </div>
   );
 };
