7793da8bb5e6376251b8a8134c073ad8fb94ac2a
juanrm29 <juanmaulana29@gmail.com>
2024-01-20 03:43:36 +0800

    fix auth

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import { SignInButton } from "@clerk/clerk-react";
+import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
-import { useConvexAuth } from "convex/react";
 import { Spinner } from "@/components/spinner";
 
 export const Heading = () => {
@@ -12,22 +14,33 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
-        <span className="underline">Jution</span>
+        Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Jotion</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-        Jution is the connected workspace where <br />
+        Jotion is the connected workspace where <br />
         better, faster work happens.
       </h3>
       {isLoading && (
-        <Spinner size="lg"/>
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Jotion
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
       )}
       {!isAuthenticated && !isLoading && (
-      <Button>
-        Enter Jution
-        <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+        <SignInButton mode="modal">
+          <Button>
+            Get Jotion free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
       )}
     </div>
-  );
-};
+  )
+}
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://summary-pony-72.clerk.accounts.dev",
+        applicationID: "convex",
+      }
+    ]
+  }
