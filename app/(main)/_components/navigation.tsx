b0f5420d73cd0f0c9b56dab2ed0ea77f82bb21cf
juanrm29 <juanmaulana29@gmail.com>
2024-01-21 01:59:30 +0800

    sidebar-update

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 
-import { ChevronsLeft, ChevronsLeftIcon, MenuIcon, PlusCircle, Search} from "lucide-react";
+import { ChevronsLeft, ChevronsLeftIcon, MenuIcon, PlusCircle, Search, Settings} from "lucide-react";
 import { useParams, usePathname, useRouter } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -137,6 +137,11 @@ export const Navigation = () => {
             label="Search"
             icon={Search}
             isSearch
+            onClick={() => {}}
+          />
+              <Item
+            label="Settings"
+            icon={Settings}
             onClick={() => {}}
           />
         <Item
