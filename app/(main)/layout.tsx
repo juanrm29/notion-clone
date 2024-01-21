ba3df330581cfb5da68b3021343b4e27fedcdd65
juanrm29 <juanmaulana29@gmail.com>
2024-01-21 13:56:18 +0800

    search function

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,11 +8,7 @@ import { SearchCommand } from "@/components/search-command";
 
 import { Navigation } from "./_components/navigation";
 
-const MainLayout = ({
-  children
-}: {
-  children: React.ReactNode;
-}) => {
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
 
   if (isLoading) {
@@ -27,14 +23,15 @@ const MainLayout = ({
     return redirect("/");
   }
 
-  return ( 
+  return (
     <div className="h-full flex dark:bg-[#1F1F1F]">
       <Navigation />
       <main className="flex-1 h-full overflow-y-auto">
+        <SearchCommand />
         {children}
       </main>
     </div>
-   );
-}
- 
+  );
+};
+
 export default MainLayout;
